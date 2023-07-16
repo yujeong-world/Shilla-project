f40106c06447d213a8ff36dc654d134a9f806ca0
yujeong-world <kyjii082@gamil.com>
2023-07-16 11:45:23 +0900

    불필요파일 삭제 및 정리

--- wedding/js/nav.js
+++ wedding/js/nav.js
@@ -3,6 +3,7 @@ var menu_list = menu.children("li"); //ul하위메뉴 li 1차메뉴
 /* menu_list.css('background-color', 'red');
 console.log(menu_list);
  */
+menu_list.children("div").css("display","none");
 menu_list.on("mouseenter", function () {
 	var target = $(this);
 	target.addClass("slide");
